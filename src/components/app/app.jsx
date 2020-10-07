317a27faa4202a5663df2f25788b15603f3609a5
DmitryPateew <60105448+DmitryPateew@users.noreply.github.com>
2020-10-07 17:44:24 +0300

    add a few pages

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "../mainPage/mainPage";
+import LoginPage from "../loginPage/loginPage";
+import OfferPage from "../offerPge/offerPage";
+import FavoritePage from "../favoritePage/favoritePage";
 
 const App = (props) => {
   const {quantityOffer} = props;
 
   return (
-    <MainPage quantityOffer={quantityOffer} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage quantityOffer={quantityOffer}/>
+        </Route>
+        <Route exact path="/login">
+          <LoginPage/>
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritePage/>
+        </Route>
+        <Route exact path="/offer/:id">
+          <OfferPage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
@@ -15,3 +34,4 @@ App.propTypes = {
 };
 
 export default App;
+
